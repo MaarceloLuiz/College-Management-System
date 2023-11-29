b84249625eafb65340dbbe6c38cfe2b8d3d7d09a
MaarceloLuiz <marceloluizgpereira@gmail.com>
2023-11-29 10:20:46 +0000

    DAO and Person abstract class

---
+++ CA1 College/Person.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CA1_College
+{
+    public abstract class Person
+    {
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public string County { get; set; }
+        public int Age { get; set; }
+        public string Phone { get; set; }
+        public string Email { get; set; }
+
+        public abstract void AddPerson();
+
+    }
+}
