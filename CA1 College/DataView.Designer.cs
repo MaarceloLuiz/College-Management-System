109e401502ef8934c4e23d7d6cf598dfab3da4e9
MaarceloLuiz <marceloluizgpereira@gmail.com>
2023-11-29 19:52:31 +0000

    Show Data Page updated && new stored procs added

--- CA1 College/DataView.Designer.cs
+++ CA1 College/DataView.Designer.cs
@@ -31,14 +31,15 @@
             this.DGVCollege = new System.Windows.Forms.DataGridView();
             this.btnShowAge = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
-            this.btnShowAll = new System.Windows.Forms.Button();
-            this.cboShowAge = new System.Windows.Forms.ComboBox();
+            this.btnShowAllStudents = new System.Windows.Forms.Button();
             this.cboShowGender = new System.Windows.Forms.ComboBox();
             this.btnShowGender = new System.Windows.Forms.Button();
             this.cboShowCourse = new System.Windows.Forms.ComboBox();
             this.btnShowCourse = new System.Windows.Forms.Button();
             this.cboShowModule = new System.Windows.Forms.ComboBox();
             this.btnShowModule = new System.Windows.Forms.Button();
+            this.txtShowAge = new System.Windows.Forms.TextBox();
+            this.btnShowAllLecturers = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.DGVCollege)).BeginInit();
             this.SuspendLayout();
             // 
@@ -71,22 +72,15 @@
             this.label1.TabIndex = 3;
             this.label1.Text = "Show Data";
             // 
-            // btnShowAll
+            // btnShowAllStudents
             // 
-            this.btnShowAll.Location = new System.Drawing.Point(12, 131);
-            this.btnShowAll.Name = "btnShowAll";
-            this.btnShowAll.Size = new System.Drawing.Size(158, 52);
-            this.btnShowAll.TabIndex = 4;
-            this.btnShowAll.Text = "Show All";
-            this.btnShowAll.UseVisualStyleBackColor = true;
-            // 
-            // cboShowAge
-            // 
-            this.cboShowAge.FormattingEnabled = true;
-            this.cboShowAge.Location = new System.Drawing.Point(389, 147);
-            this.cboShowAge.Name = "cboShowAge";
-            this.cboShowAge.Size = new System.Drawing.Size(172, 24);
-            this.cboShowAge.TabIndex = 45;
+            this.btnShowAllStudents.Location = new System.Drawing.Point(12, 131);
+            this.btnShowAllStudents.Name = "btnShowAllStudents";
+            this.btnShowAllStudents.Size = new System.Drawing.Size(158, 52);
+            this.btnShowAllStudents.TabIndex = 4;
+            this.btnShowAllStudents.Text = "Show All Students";
+            this.btnShowAllStudents.UseVisualStyleBackColor = true;
+            this.btnShowAllStudents.Click += new System.EventHandler(this.btnShowAllStudents_Click);
             // 
             // cboShowGender
             // 
@@ -112,6 +106,7 @@
             this.cboShowCourse.Name = "cboShowCourse";
             this.cboShowCourse.Size = new System.Drawing.Size(172, 24);
             this.cboShowCourse.TabIndex = 51;
+            this.cboShowCourse.SelectedIndexChanged += new System.EventHandler(this.cboShowCourse_SelectedIndexChanged);
             // 
             // btnShowCourse
             // 
@@ -139,24 +134,43 @@
             this.btnShowModule.Text = "Show Module";
             this.btnShowModule.UseVisualStyleBackColor = true;
             // 
+            // txtShowAge
+            // 
+            this.txtShowAge.Location = new System.Drawing.Point(389, 146);
+            this.txtShowAge.Name = "txtShowAge";
+            this.txtShowAge.Size = new System.Drawing.Size(172, 22);
+            this.txtShowAge.TabIndex = 52;
+            // 
+            // btnShowAllLecturers
+            // 
+            this.btnShowAllLecturers.Location = new System.Drawing.Point(12, 73);
+            this.btnShowAllLecturers.Name = "btnShowAllLecturers";
+            this.btnShowAllLecturers.Size = new System.Drawing.Size(158, 52);
+            this.btnShowAllLecturers.TabIndex = 53;
+            this.btnShowAllLecturers.Text = "Show All Lecturers";
+            this.btnShowAllLecturers.UseVisualStyleBackColor = true;
+            this.btnShowAllLecturers.Click += new System.EventHandler(this.btnShowAllLecturers_Click);
+            // 
             // DataView
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(976, 548);
+            this.Controls.Add(this.btnShowAllLecturers);
+            this.Controls.Add(this.txtShowAge);
             this.Controls.Add(this.cboShowCourse);
             this.Controls.Add(this.btnShowCourse);
             this.Controls.Add(this.cboShowModule);
             this.Controls.Add(this.btnShowModule);
             this.Controls.Add(this.cboShowGender);
             this.Controls.Add(this.btnShowGender);
-            this.Controls.Add(this.cboShowAge);
-            this.Controls.Add(this.btnShowAll);
+            this.Controls.Add(this.btnShowAllStudents);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.btnShowAge);
             this.Controls.Add(this.DGVCollege);
             this.Name = "DataView";
             this.Text = "DataView";
+            this.Load += new System.EventHandler(this.DataView_Load);
             ((System.ComponentModel.ISupportInitialize)(this.DGVCollege)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
@@ -168,13 +182,14 @@
         private System.Windows.Forms.DataGridView DGVCollege;
         private System.Windows.Forms.Button btnShowAge;
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Button btnShowAll;
-        private System.Windows.Forms.ComboBox cboShowAge;
+        private System.Windows.Forms.Button btnShowAllStudents;
         private System.Windows.Forms.ComboBox cboShowGender;
         private System.Windows.Forms.Button btnShowGender;
         private System.Windows.Forms.ComboBox cboShowCourse;
         private System.Windows.Forms.Button btnShowCourse;
         private System.Windows.Forms.ComboBox cboShowModule;
         private System.Windows.Forms.Button btnShowModule;
+        private System.Windows.Forms.TextBox txtShowAge;
+        private System.Windows.Forms.Button btnShowAllLecturers;
     }
 }
--- CA1 College/DataView.cs
+++ CA1 College/DataView.cs
@@ -1,7 +1,9 @@
-﻿using System;
+﻿using Bank___Week_5_class;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -15,6 +17,54 @@ namespace CA1_College
         public DataView()
         {
             InitializeComponent();
+        }
+
+        DAO dao = new DAO();
+        SqlDataAdapter da;
+        DataTable dt;
+
+        private void DataView_Load(object sender, EventArgs e)
+        {
+            string proc = "ProcGetAll";
+            LoadData(proc);
+
+            cboShowGender.DataSource = Enum.GetValues(typeof(Gender));
+            cboShowCourse.DataSource = Enum.GetValues(typeof(Course));
+        }
+
+        void LoadData(string proc)
+        {
+            da = new SqlDataAdapter();
+            dt = new DataTable();
+
+            SqlCommand cmd = dao.OpenCon().CreateCommand();
+            cmd.CommandText= proc;
+            cmd.CommandType= CommandType.StoredProcedure;
+
+            da.SelectCommand = cmd;
+            da.Fill(dt);
+            DGVCollege.DataSource = dt;
+
+            dao.CloseCon();
+        }
+
+        private void cboShowCourse_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (cboShowCourse.SelectedIndex.Equals(0)) cboShowModule.DataSource = Enum.GetValues(typeof(ModuleCS));
+            if (cboShowCourse.SelectedIndex.Equals(1)) cboShowModule.DataSource = Enum.GetValues(typeof(ModulePSY));
+            if (cboShowCourse.SelectedIndex.Equals(2)) cboShowModule.DataSource = Enum.GetValues(typeof(ModuleHis));
+        }
+
+        private void btnShowAllLecturers_Click(object sender, EventArgs e)
+        {
+            string proc = "ProcGetAllLecturer";
+            LoadData(proc);
+        }
+
+        private void btnShowAllStudents_Click(object sender, EventArgs e)
+        {
+            string proc = "ProcGetAllStudent";
+            LoadData(proc);
         }
     }
 }
